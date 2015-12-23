d8cd04f7240fdb0d3f8a7bd071d37cf5c6905038
Ginger <wjz416@gmail.com>
2015-12-23 21:37:07 +0800

    First Done.

--- testPyLuaTblParser.py
+++ testPyLuaTblParser.py
@@ -8,10 +8,9 @@ a3 = PyLuaTblParser()
 #test_str = '{array = {65,23,5,},dict = {mixed = {43,54.33,false,9,string = "value",},array = {3,6,4,},string = "value",},}'
 f = open('test.txt')
 test_str = f.read()
-print test_str
 f.close()
 
-time.sleep(1)
+time.sleep(0.1)
 
 print 'a1.dict:'
 a1.load(test_str)
